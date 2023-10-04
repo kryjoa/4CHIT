29b90c15d215c642846078e140a91bb4125d6468
Moritz Greiner <m.greiner@htlkrems.at>
2023-10-04 16:11:02 +0200

    Racecar funktioniert

--- RacingCar/RacingCar/Program.cs
+++ RacingCar/RacingCar/Program.cs
@@ -9,23 +9,21 @@
     {
         Car car1 = new Car("VER");
         new Thread(car1.Run).Start();
+        
         Car car2 = new Car("LEC");
         new Thread(car2.Run).Start();
+        
         Car car3 = new Car("NOR");
         new Thread(car3.Run).Start();
+        
         Car car4 = new Car("HUL");
         new Thread(car4.Run).Start();
+        
         Car car5 = new Car("HAM");
         new Thread(car5.Run).Start();
-
-        static void main()
-        {
-            Car car1 = new Car("VER");
-            Car car2 = new Car("LEC");
-            Car car3 = new Car("NOR");
-            Car car4 = new Car("HUL");
-            Car car5 = new Car("HAM");
-        }
+        
+        F1Race austria = new F1Race();
+        new Thread(austria.Run).Start();
     }
 
     public class Car {
@@ -41,7 +39,7 @@
         race.Wait();
         Race();
         box.Wait();
-        TakingPistop();
+        TakingPitstop();
         box.Release();
         Race();
         end.Release();
@@ -56,7 +54,7 @@
         Console.WriteLine($"{Racer}: Racing");
         Thread.Sleep(1500);
         }
-    private void TakingPistop(){
+    private void TakingPitstop(){
         Console.WriteLine(
             $"{Racer}: Taking Pit stop");
         Thread.Sleep(500);
@@ -69,14 +67,14 @@
         {
             start.Wait();
             Start();
-            race.Release();
+            race.Release(5);
             end.Wait();
             End();
         }
 
         private void Start(){
-        Console.WriteLine("Starting Race");
-        Thread.Sleep(1000);
+            Console.WriteLine("Starting Race");
+            Thread.Sleep(1000);
         }
     
     private void End(){
