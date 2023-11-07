bd300262b694978c97404081064f9961cbe7fb82
Moritz Greiner <m.greiner@htlkrems.at>
2023-11-07 10:08:42 +0100

    StarWars API

---
+++ SwAPI/SwAPI/SwAPIClasses.cs
@@ -0,0 +1,10 @@
+﻿namespace SwAPI.Classes;
+
+public class Character
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Faction { get; set; }
+    public string  Homeworld { get; set; }
+    public string Species { get; set; }
+}
